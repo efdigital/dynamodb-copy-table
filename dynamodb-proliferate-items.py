447ea5436baf189534abb2dd41859aa0359e7357
Robert Bragg <robertbragg@theambassadors.com>
2023-09-04 10:15:38 +0100

    Progressbar to separate file

--- dynamodb-proliferate-items.py
+++ dynamodb-proliferate-items.py
@@ -3,28 +3,7 @@ from math import ceil
 import argparse
 from uuid import uuid4
 import boto3
-
-# Print iterations progress
-def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
-    """
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    filledLength = int(length * iteration // total)
-    bar = fill * filledLength + '-' * (length - filledLength)
-    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
-    # Print New Line on Complete
-    if iteration == total: 
-        print()
+from progressbar import printProgressBar
 
 # Generate records
 def proliferate(table, primaryKey, secondaryKey, quantity, region):
